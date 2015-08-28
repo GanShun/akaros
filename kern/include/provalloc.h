fcd7c03e8bc6eb0d393af1b2fd15065cd00b3fe7
Valmon Leymarie <leymariv@berkeley.edu>
2015-08-28 15:05:18 -0700

    Move structures from provalloc.h to schedule.h
    We keep the structures Barett used in schedule.h for now, I am not sure if
    this is what we want but for now it's ok. So we basically added the fields we
    needed in schedule.h structures.

--- kern/include/provalloc.h
+++ kern/include/provalloc.h
@@ -5,24 +5,15 @@
  * See LICENSE for details.
  */
 
-#ifndef	SCHEDULE_H
-#define	SCHEDULE_H
+#ifndef	PROVALLOC_H
+#define	PROVALLOC_H
 
 #include <sys/queue.h>
 #include <arch/topology.h>
+#include <process.h>
 
 enum pnode_type { CORE, CPU, SOCKET, NUMA, MACHINE, NUM_NODE_TYPES};
 static char pnode_label[5][8] = { "CORE", "CPU", "SOCKET", "NUMA", "MACHINE" };
-
-struct sched_pcore {
-	struct sched_pnode *spn;
-	struct core_info *spc_info;
-	STAILQ_ENTRY(sched_pcore) prov_next;
-	STAILQ_ENTRY(sched_pcore) alloc_next;
-	struct proc *alloc_proc;
-	struct proc *prov_proc;
-};
-STAILQ_HEAD(sched_pcore_tailq, sched_pcore);
 
 struct sched_pnode {
 	int id;
@@ -33,21 +24,11 @@ struct sched_pnode {
 	struct sched_pcore *spc_data;
 };
 
-struct sched_proc_data {
-	struct sched_pcore_tailq alloc_me;
-	struct sched_pcore_tailq prov_alloc_me;
-	struct sched_pcore_tailq prov_not_alloc_me;
-};
-
-struct proc {
-	struct sched_proc_data ksched_data;
-};
-
-void nodes_init();
-void alloc_core_any(struct proc *p, int amt);
-void alloc_core_specific(struct proc *p, int core_id);
-int free_core_specific(struct proc *p, int core_id);
-void provision_core(struct proc *p, int core_id);
+void provalloc_nodes_init();
+struct sched_pcore *provalloc_alloc_core(struct proc *p);
+void provalloc_track_alloc(struct proc *p, struct sched_pcore *c);
+void provalloc_free_core(struct proc *p, uint32_t core_id);
+void provalloc_prov_core(struct proc *p, uint32_t core_id);
 
 void print_node(struct sched_pnode *n);
 void print_nodes(int type);
--- kern/include/schedule.h
+++ kern/include/schedule.h
@@ -19,6 +19,8 @@ TAILQ_HEAD(proc_list, proc);		/* Declares 'struct proc_list' */
  * the prov_proc and alloc_proc are weak (internal) references, and should only
  * be used as a ref source while the ksched has a valid kref. */
 struct sched_pcore {
+	struct sched_pnode          *spn;
+	struct core_info            *spc_info;
 	TAILQ_ENTRY(sched_pcore)	prov_next;			/* on a proc's prov list */
 	TAILQ_ENTRY(sched_pcore)	alloc_next;			/* on an alloc list (idle)*/
 	struct proc					*prov_proc;			/* who this is prov to */
@@ -30,6 +32,7 @@ TAILQ_HEAD(sched_pcore_tailq, sched_pcore);
 struct sched_proc_data {
 	TAILQ_ENTRY(proc)			proc_link;			/* tailq linkage */
 	struct proc_list 			*cur_list;			/* which tailq we're on */
+	struct sched_pcore_tailq    alloc_me;
 	struct sched_pcore_tailq	prov_alloc_me;		/* prov cores alloced us */
 	struct sched_pcore_tailq	prov_not_alloc_me;	/* maybe alloc to others */
 	/* count of lists? */
